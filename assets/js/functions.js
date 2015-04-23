e8ee67c5eebcc3e97c2d92374920859be5afba84
Anton Kann <a.kann@intrallect.com>
2015-04-23 17:00:34 +0100

    projects section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   // smooth scrolling to the hashed locations ('#about', '#clients', etc)
   smoothScroll(1000);
+  slider()
 });
 
   function smoothScroll (duration) {
@@ -16,4 +17,13 @@ $(document).ready(function() {
         }, duration);
       }
     });
+  }
+
+  function slider() {
+    $('.thumb-unit').click(function() {
+      $('.projects-slider').css('left', '-100%');
+    })
+    $('.icon-return').click(function() {
+      $('.projects-slider').css('left', '0%%');
+    })
   }
