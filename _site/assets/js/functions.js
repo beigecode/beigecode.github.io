1333b5a7a6bc26d11fa243b16518b9e7c3c2de5f
Anton Kann <a.kann@intrallect.com>
2015-04-28 15:41:02 +0100

    finished projects section

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
   // smooth scrolling to the hashed locations ('#about', '#clients', etc)
   smoothScroll(1000);
-  slider()
+  slider();
+  loadProjects();
 });
 
   function smoothScroll (duration) {
@@ -22,8 +23,24 @@ $(document).ready(function() {
   function slider() {
     $('.thumb-unit').click(function() {
       $('.projects-slider').css('left', '-100%');
+      $('.projects-container').show();
     })
     $('.icon-return').click(function() {
-      $('.projects-slider').css('left', '0%%');
+      $('.projects-slider').css('left', '0%');
+      $('.projects-container').hide(800);
     })
+  };
+
+  function loadProjects() {
+
+    $.ajaxSetup ({ cache: true });
+    $('.thumb-unit').click(function() {
+    var $this = $(this),
+        newTitle = $this.find('strong').text(),
+        newFolder = $this.data('folder'),
+        spinner = '<div class="loader">Loading...</div>',
+        newHTML = '/projects/'+ newFolder +'.html';
+        $('.project-load').html(spinner).load(newHTML);
+        $('.project-title').text(newTitle);
+    });
   }
